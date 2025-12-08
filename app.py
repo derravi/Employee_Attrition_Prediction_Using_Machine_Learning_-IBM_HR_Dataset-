f056a9d2e47581a372fde6289a88094fbc81a21c
Ravi Der <derravi00@gmail.com>
2025-12-08 23:05:48 +0530

    FastApis add

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from Schema.model_pydantic import UserInput
+from fastapi.responses import JSONResponse
 import pandas as pd
 import pickle
 
 #Recive the Models.
-with open("/Models/model.pkl","rb") as f:
+with open("Models/model.pkl","rb") as f:
     models = pickle.load(f)
 
 lr = models['linearRegre']
@@ -23,7 +24,7 @@ def default():
 
 @app.post("/predict")
 def prediction(predict:UserInput):
-    dsta = pd.DataFrame({
+    data = pd.DataFrame([{
     'Age': predict.Age,
     'BusinessTravel': predict.BusinessTravel,
     'DailyRate': predict.DailyRate,
@@ -54,4 +55,34 @@ def prediction(predict:UserInput):
     'YearsInCurrentRole': predict.YearsInCurrentRole,
     'YearsSinceLastPromotion': predict.YearsSinceLastPromotion,
     'YearsWithCurrManager': predict.YearsWithCurrManager
-    })
+    }])
+
+    #Lets encode the all the Labeled Data Columns
+    for i in temp1:
+        data[i] = encode[i].transform(data[i])
+    
+    #Lets Scaldown the All the Data
+    scaled_data = std.transform(data)
+
+    #Lets use the Linear Regressor model
+    lr_predict = lr.predict(scaled_data)
+    xgboost_predict = xgb.predict(scaled_data)
+
+    lr_class = int(round(lr_predict[0]))
+    xgb_class = int(round(xgboost_predict[0]))
+
+    attrition_encoder = encode['Attrition']
+
+    #Lets invers transform
+    lr_final = attrition_encoder.inverse_transform([lr_class])[0]
+    xgb_final = attrition_encoder.inverse_transform([xgb_class])[0]
+
+    lr_ans = "Employee is likely to LEAVE the company." if lr_final == "Yes" else "Employee is likely to STAY in the company."
+    xgb_ans = "Employee is likely to LEAVE the company." if xgb_final == "Yes" else "Employee is likely to STAY in the company."
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "The prediction of the Linear Regressor is":lr_ans,
+            "The prediction of the XGBoost is":xgb_ans
+        })
